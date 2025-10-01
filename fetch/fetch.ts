e817bf2515daf5975dfe4a9f57a1a97ba8352721
Gerhard Meyer <gerhardm@pragmaworld.net>
2025-10-01 11:57:59 +0200

    add exercises

---
+++ fetch/fetch.ts
@@ -0,0 +1,11 @@
+// Create a function called fetchJson() 
+// The function must allow generic types
+// The function must return the data as a Promise of the generic type
+// it must use the fetch API. Don't worry about the detail of the fetch api
+// e.g.   const response = await fetch(url);
+//        const data = await response.json();
+
+
+
+// const workOrders = await fetchJson<WorkOrder[]>("/api/work-orders");
+// const regularAssets = await fetchJson<RegularAsset[]>("/api/regular-assets");
---
+++ stack/stack.ts
@@ -0,0 +1,5 @@
+// Create a class called "Stack" that contains a private array called "items".
+// The stack must be constrcuted with a specific type, but it supports any type.
+// The class must have the following methods:
+// - push(item: T): void - adds an item to the top of the stack
+// - pop(): T | undefined - removes and returns the item from the top of the stack
